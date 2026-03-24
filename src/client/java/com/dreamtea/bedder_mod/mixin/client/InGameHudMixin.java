6a9b80d0ffcf8ce5afe4b91d8a95603a7564f762
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2026-03-24 17:20:14 -0400

    Switch to non-obfuscated "mappings"

--- src/client/java/com/dreamtea/bedder_mod/mixin/client/InGameHudMixin.java
+++ src/client/java/com/dreamtea/bedder_mod/mixin/client/InGameHudMixin.java
@@ -2,50 +2,50 @@ package com.dreamtea.bedder_mod.mixin.client;
 
 import com.dreamtea.bedder_mod.SpamTracker;
 import com.dreamtea.bedder_mod.imixin.ISpamClick;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.text.Text;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(InGameHud.class)
+@Mixin(Gui.class)
 public abstract class InGameHudMixin {
 
-    @Shadow @Final private MinecraftClient client;
+    @Shadow @Final private Minecraft minecraft;
 
     @Redirect(
             method = "renderOverlayMessage",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithBackground(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIII)V"
+                    target = "Lnet/minecraft/client/gui/GuiGraphics;drawStringWithBackdrop(Lnet/minecraft/client/gui/Font;Lnet/minecraft/network/chat/Component;IIII)V"
             )
     )
     public void adjustFont(
-            DrawContext instance,
-            TextRenderer textRenderer,
-            Text text,
+            GuiGraphics instance,
+            Font textRenderer,
+            Component text,
             int x,
             int y,
             int width,
             int color,
-            DrawContext context,
-            RenderTickCounter tickCounter
+            GuiGraphics context,
+            DeltaTracker tickCounter
     ){
         int drawColor = color;
-        if(this.client.player instanceof ISpamClick isc) {
+        if(this.minecraft.player instanceof ISpamClick isc) {
             SpamTracker spam = isc.getClicker();
             spam.setSpammedText(text);
             if(isc.getClicker().active()){
-                spam.matricesTranslate(context.getMatrices());
+                spam.matricesTranslate(context.pose());
                 drawColor = isc.getClicker().getColor();
             }
         }
-        context.drawTextWithBackground(textRenderer, text, x, y, width, drawColor);
+        context.drawStringWithBackdrop(textRenderer, text, x, y, width, drawColor);
     }
 }
